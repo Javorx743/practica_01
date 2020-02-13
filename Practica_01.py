b6ebad8d73e752ad11f45910d2c3b2dd5995c586
Javorx743 <60306016+Javorx743@users.noreply.github.com>
2020-02-12 21:49:33 -0600

    Add files via upload

---
+++ Practica_01.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  4 09:14:30 2020
+
+@author: ALUMNO
+"""
+
+names=['Luis', 'Tocayo', 'Gaudi', 'Didier', 'Ariel']
+print (names)
+print (names[0]+' Dame dulces')
+print (names[1]+' Esperame 1 semana màs')
+print (names[2]+' Ya vamonooos')
+print (names[3]+' ¿Cuándo nos vamos a ver?')
+print (names[4]+' Hola Arielito')
+
+
+print'Lista de deseos'
+a=0
+while a<16:
+    b=(input('¿Cuál es tu deseo?'))
+    print b
+a+=1
